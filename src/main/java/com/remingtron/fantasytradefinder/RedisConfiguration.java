acbb89e9d5326c73ec074f2b5050410a169e4fe9
Remington Davenport <remington.davenport@gmail.com>
2018-11-29 00:13:51 -0500

    reading env var if present

--- src/main/java/com/remingtron/fantasytradefinder/RedisConfiguration.java
+++ src/main/java/com/remingtron/fantasytradefinder/RedisConfiguration.java
@@ -2,19 +2,31 @@ package com.remingtron.fantasytradefinder;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfiguration {
 
     @Bean
-    public JedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory redisConnectionFactory() throws URISyntaxException {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        String redisUrlEnvironmentVariable = System.getenv("REDIS_URL");
+        if (!StringUtils.isEmpty(redisUrlEnvironmentVariable)) {
+            URI redisUrl = new URI(redisUrlEnvironmentVariable);
+            redisStandaloneConfiguration.setHostName(redisUrl.getHost());
+            redisStandaloneConfiguration.setPort(redisUrl.getPort());
+        }
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
